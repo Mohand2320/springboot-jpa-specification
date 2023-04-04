69a4b6e3139c41390946b61dbcae7e1a3fec7686
Mohand2320 <mohandamoua.enageo@gmail.com>
2023-04-04 10:12:29 +0200

    add all

---
+++ src/main/java/com/piinalpin/queryrequest/domain/common/query/FilterRequest.java
@@ -0,0 +1,42 @@
+package com.piinalpin.queryrequest.domain.common.query;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+/**
+ * l'annotation @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+ * est une annotation Jackson qui indique que le format des noms de propriétés
+ * de la classe doit être converti en casse de serpent.
+ * Par exemple, le nom de propriété valueTo sera sérialisé en value_to.
+ */
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class FilterRequest implements Serializable {
+
+    private static final long serialVersionUID = 6293344849078612450L;
+
+    private String key;
+
+    private Operator operator;
+
+    private FieldType fieldType;
+
+    private transient Object value;
+
+    private transient Object valueTo;
+
+    private transient List<Object> values;
+
+}
